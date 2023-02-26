e4dce3c14026f383e8e59a0afa4bf0a3291af8e1
Thomas Wallingford <tbwallingford@gmail.com>
2023-02-26 16:10:21 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+
+    return (fetch("https://anapioficeandfire.com/api/books"))
+    };
 
 function renderBooks(books) {
   const main = document.querySelector('main');
